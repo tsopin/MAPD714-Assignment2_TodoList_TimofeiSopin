cee1a4e6912adf79831b5383c381173f072be552
Timofei Sopin <timofei.sopin@gmail.com>
2017-12-14 15:09:12 -0500

    Pods created

---
+++ Pods/Target Support Files/Pods-MAPD714-Assignment2_TodoList_TimofeiSopin/Pods-MAPD714-Assignment2_TodoList_TimofeiSopin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MAPD714_Assignment2_TodoList_TimofeiSopin : NSObject
+@end
+@implementation PodsDummy_Pods_MAPD714_Assignment2_TodoList_TimofeiSopin
+@end
